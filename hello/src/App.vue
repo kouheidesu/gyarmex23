ab348b62d74f460122ba018f58930c96f1af7337
kouheidesu <gyarmex@gmail.com>
2021-07-31 13:20:24 +0900

    first-commit

---
+++ hello/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to COACHTECH"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+<template>
+<div id="app">
+<input type="text" v-model="text"/>
+<p>{{text}}</p>
+</div>
+</template>
+
+<script>
+export default{
+  data(){
+    return{
+      text:""
+    };
+  }
+};
+</script>
+<style>
+
+</style>
